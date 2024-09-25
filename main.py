df5574dcc5951eef60a9396e4d3571c6469883b4
Relevant Online <developers@relevantonline.eu>
2024-09-25 10:44:27 +0200

    Made base setup for blackjack game

--- main.py
+++ main.py
@@ -1,81 +1,46 @@
-# Code here
-CARD_VALUES = [
-    # Aces
-    [
-        ".-------.\n|A      |\n|   ♠   |\n|      A|\n'-------'",  # Ace of Spades
-        ".-------.\n|A      |\n|   ♥   |\n|      A|\n'-------'"   # Ace of Hearts
-    ],
+#Variables
+#Dealer
+currentDealerHand = 0
 
-    # 2s
-    [
-        ".-------.\n|2      |\n|   ♠   |\n|      2|\n'-------'",  # 2 of Spades
-        ".-------.\n|2      |\n|   ♥   |\n|      2|\n'-------'"   # 2 of Hearts
-    ],
+#Player
+playerBalance = 0
+currentPlayerHand = 0
+hitOrFoldChoice = ""
 
-    # 3s
-    [
-        ".-------.\n|3      |\n|   ♠   |\n|      3|\n'-------'",  # 3 of Spades
-        ".-------.\n|3      |\n|   ♥   |\n|      3|\n'-------'"   # 3 of Hearts
-    ],
+#Game
+gameIsActive = False
+winner = ""
 
-    # 4s
-    [
-        ".-------.\n|4      |\n|   ♠   |\n|      4|\n'-------'",  # 4 of Spades
-        ".-------.\n|4      |\n|   ♥   |\n|      4|\n'-------'"   # 4 of Hearts
-    ],
+#------
+def decideBetAmount():
+    print("Test")
 
-    # 5s
-    [
-        ".-------.\n|5      |\n|   ♠   |\n|      5|\n'-------'",  # 5 of Spades
-        ".-------.\n|5      |\n|   ♥   |\n|      5|\n'-------'"   # 5 of Hearts
-    ],
+    gameIsActive = True
 
-    # 6s
-    [
-        ".-------.\n|6      |\n|   ♠   |\n|      6|\n'-------'",  # 6 of Spades
-        ".-------.\n|6      |\n|   ♥   |\n|      6|\n'-------'"   # 6 of Hearts
-    ],
+def dealing():
+    print("Dealer is dealing")
 
-    # 7s
-    [
-        ".-------.\n|7      |\n|   ♠   |\n|      7|\n'-------'",  # 7 of Spades
-        ".-------.\n|7      |\n|   ♥   |\n|      7|\n'-------'"   # 7 of Hearts
-    ],
+def givePlayerInput():
+    print("Player decides hit or fold")
+    playerChoice = "" #Can be either Hit or Fold
+    return playerChoice
 
-    # 8s
-    [
-        ".-------.\n|8      |\n|   ♠   |\n|      8|\n'-------'",  # 8 of Spades
-        ".-------.\n|8      |\n|   ♥   |\n|      8|\n'-------'"   # 8 of Hearts
-    ],
+def checkForWinOrLoss():
+    print("Check for win or loss")
+    if winner == "player":
+        gameIsActive == False
+        print("Player won")
+    
+#Game Loop
+decideBetAmount()
 
-    # 9s
-    [
-        ".-------.\n|9      |\n|   ♠   |\n|      9|\n'-------'",  # 9 of Spades
-        ".-------.\n|9      |\n|   ♥   |\n|      9|\n'-------'"   # 9 of Hearts
-    ],
+while gameIsActive == True:
+    dealing()
+    
+    checkForWinOrLoss()
 
-    # 10s
-    [
-        ".-------.\n|10     |\n|   ♠   |\n|     10|\n'-------'",  # 10 of Spades
-        ".-------.\n|10     |\n|   ♥   |\n|     10|\n'-------'"   # 10 of Hearts
-    ],
-
-    # Jacks
-    [
-        ".-------.\n|J      |\n|   ♠   |\n|      J|\n'-------'",  # Jack of Spades
-        ".-------.\n|J      |\n|   ♥   |\n|      J|\n'-------'"   # Jack of Hearts
-    ],
-
-    # Queens
-    [
-        ".-------.\n|Q      |\n|   ♠   |\n|      Q|\n'-------'",  # Queen of Spades
-        ".-------.\n|Q      |\n|   ♥   |\n|      Q|\n'-------'"   # Queen of Hearts
-    ],
-
-    # Kings
-    [
-        ".-------.\n|K      |\n|   ♠   |\n|      K|\n'-------'",  # King of Spades
-        ".-------.\n|K      |\n|   ♥   |\n|      K|\n'-------'"   # King of Hearts
-    ]
-]
-
+    hitOrFoldChoice = givePlayerInput()
+    if (hitOrFoldChoice) == "Hit":
+        dealing()
+    elif (hitOrFoldChoice) == "Fold":
+        checkForWinOrLoss()
